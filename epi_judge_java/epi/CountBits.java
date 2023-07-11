166c4bd07735231f475e688f53097d7accbb3234
Piyush Mourya <mauryapiyush76@gmail.com>
2023-07-11 13:12:17 +0530

    Solved CountBits with loop and modulo function

--- epi_judge_java/epi/CountBits.java
+++ epi_judge_java/epi/CountBits.java
@@ -1,19 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class CountBits {
-  @EpiTest(testDataFile = "count_bits.tsv")
+    @EpiTest(testDataFile = "count_bits.tsv")
 
-  public static short countBits(int x) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    public static short countBits(int x) {
+        short count = 0;
+        while (x != 0) {
+            if (x % 2 == 1) {
+                count++;
+            }
+            x = x / 2;
+        }
+        return count;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "CountBits.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "CountBits.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
